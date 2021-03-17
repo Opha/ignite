af049c155fce4edd376f0bbb1da1851998d7e6b7
Opha <avetisianophelia@gmail.com>
2021-03-17 15:18:34 +0400

    initial commit

---
+++ src/actions/gamesAction.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { popularGamesURL, upcomingGamesURL, newGamesURL } from "../api";
+
+// import axios from "axios";
+
+export const loadGames = () => async (dispatch) => {
+  const popularGamesData = await axios.get(popularGamesURL());
+  const newGamesData = await axios.get(newGamesURL());
+  const upcomingGamesData = await axios.get(upcomingGamesURL());
+
+  dispatch({
+    type: "FETCH_GAMES",
+    payload: {
+      popular: popularGamesData.data.results,
+      upcoming: upcomingGamesData.data.results,
+      newGames: newGamesData.data.results,
+    },
+  });
+};
+
+loadGames();
